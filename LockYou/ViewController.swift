b4edf7da5583d82bd8bb9a6f8563dfb455673c20
HanGil Kim <rlagksrlf1@sogang.ac.kr>
2019-06-12 20:54:57 +0900

    Initial Commit

---
+++ LockYou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LockYou
+//
+//  Created by sgcs on 12/06/2019.
+//  Copyright © 2019 KwangHee Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
