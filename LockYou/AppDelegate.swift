f164ffd3aa5632a001af7a021503d735c0b3ca96
KyungKoo Kang <coush123@gmail.com>
2019-06-17 04:10:57 +0900

    탭완성 및 약간 수정

--- LockYou/AppDelegate.swift
+++ LockYou/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-
+    
         print(1)
     }
 
@@ -35,6 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if (timerData.timerIsOn){
             timerData.appOutTogle = false
         }
+        
         if let temp = soundEffect {
             temp.pause()
         }
