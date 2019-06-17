b3d465c23b2273a3d33b40d330457777b7fbad90
Pinelover <pineleaf1215@gmail.com>
2019-06-17 15:17:54 +0900

    history구조체에 weekday까지 넣었음

--- LockYou/AppDelegate.swift
+++ LockYou/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -23,8 +23,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
-    
-        print(1)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -39,18 +37,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let temp = soundEffect {
             temp.pause()
         }
-        print("home")
     }
 
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        print(3)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print(4)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
